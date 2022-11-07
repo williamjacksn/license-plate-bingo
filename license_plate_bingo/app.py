c87f90d50206bfe6d84ebe46c6e894be1b9aa179
William Jackson <565174+williamjacksn@users.noreply.github.com>
2022-11-07 14:17:11 -0600

    Playable game

--- license_plate_bingo/app.py
+++ license_plate_bingo/app.py
@@ -1,6 +1,7 @@
 import flask
 import license_plate_bingo.db
 import os
+import uuid
 import waitress
 import werkzeug.middleware.proxy_fix
 
@@ -9,7 +10,9 @@ __version__ = '2022.0'
 app = flask.Flask(__name__)
 app.wsgi_app = werkzeug.middleware.proxy_fix.ProxyFix(app.wsgi_app, x_port=1)
 
-db = license_plate_bingo.db.Database(os.getenv('DB'))
+
+def get_db() -> license_plate_bingo.db.Database:
+    return license_plate_bingo.db.Database(os.getenv('DB'))
 
 
 @app.before_request
@@ -25,17 +28,53 @@ def index():
     return flask.render_template('index.html')
 
 
-@app.get('/play/game-id')
-def play():
-    flask.g.states = [
-        'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA',
-        'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK',
-        'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY',
-    ]
+@app.post('/update')
+def update():
+    for k, v in flask.request.values.lists():
+        app.logger.debug(f'{k}: {v}')
+    game_id = flask.request.values.get('game-id')
+    db = get_db()
+    game = db.games_get(game_id)
+    if game is None:
+        app.logger.warning(f'Invalid game ID: {game_id}')
+        return flask.abort(404)
+    state = flask.request.values.get('state')
+    if state in license_plate_bingo.db.STATE_NAME_TO_ABBR:
+        action = flask.request.values.get('action')
+        found_states = game['found']
+        if action == 'add':
+            found_states.add(state)
+            db.games_update(game_id, found_states)
+        elif action == 'remove':
+            found_states.discard(state)
+            db.games_update(game_id, found_states)
+        else:
+            app.logger.warning(f'Invalid game update action: {action}')
+    else:
+        app.logger.warning(f'Invalid state: {state}')
+    response = flask.make_response('', 204)
+    response.headers['HX-Refresh'] = 'true'
+    return response
+
+
+@app.get('/new')
+def new():
+    game_id = get_db().games_insert()
+    return flask.redirect(flask.url_for('play', game_id=game_id))
+
+
+@app.get('/play/<uuid:game_id>')
+def play(game_id: uuid.UUID):
+    game = get_db().games_get(game_id)
+    if game is None:
+        return flask.abort(404)
+    flask.g.game = game
+    flask.g.state_name_to_abbr = license_plate_bingo.db.STATE_NAME_TO_ABBR
     return flask.render_template('play.html')
 
 
 def main():
     app.logger.info(f'license-plate-bingo {__version__}')
-    db.db_migrate()
-    waitress.serve(app, ident=None)
+    get_db().db_migrate()
+    web_server_threads = int(os.getenv('WEB_SERVER_THREADS', 8))
+    waitress.serve(app, ident=None, threads=web_server_threads)
--- license_plate_bingo/db.py
+++ license_plate_bingo/db.py
@@ -1,8 +1,71 @@
 import datetime
 import fort
+import uuid
+
+STATE_NAME_TO_ABBR = {
+    'Alabama':       'AL', 'Alaska':      'AK', 'Arizona':        'AZ', 'Arkansas':      'AR', 'California':     'CA',
+    'Colorado':      'CO', 'Connecticut': 'CT', 'Delaware':       'DE', 'Florida':       'FL', 'Georgia':        'GA',
+    'Hawaii':        'HI', 'Idaho':       'ID', 'Illinois':       'IL', 'Indiana':       'IN', 'Iowa':           'IA',
+    'Kansas':        'KS', 'Kentucky':    'KY', 'Louisiana':      'LA', 'Maine':         'ME', 'Maryland':       'MD',
+    'Massachusetts': 'MA', 'Michigan':    'MI', 'Minnesota':      'MN', 'Mississippi':   'MS', 'Missouri':       'MO',
+    'Montana':       'MT', 'Nebraska':    'NE', 'Nevada':         'NV', 'New Hampshire': 'NH', 'New Jersey':     'NJ',
+    'New Mexico':    'NM', 'New York':    'NY', 'North Carolina': 'NC', 'North Dakota':  'ND', 'Ohio':           'OH',
+    'Oklahoma':      'OK', 'Oregon':      'OR', 'Pennsylvania':   'PA', 'Rhode Island':  'RI', 'South Carolina': 'SC',
+    'South Dakota':  'SD', 'Tennessee':   'TN', 'Texas':          'TX', 'Utah':          'UT', 'Vermont':        'VT',
+    'Virginia':      'VA', 'Washington':  'WA', 'West Virginia':  'WV', 'Wisconsin':     'WI', 'Wyoming':        'WY',
+}
+STATE_ABBR_TO_NAME = {v: k for k, v in STATE_NAME_TO_ABBR.items()}
 
 class Database(fort.SQLiteDatabase):
     _version: int = None
+
+    def games_get(self, game_id: uuid.UUID):
+        sql = '''
+            select found, game_id, started_at from games where game_id = :game_id
+        '''
+        params = {
+            'game_id': game_id,
+        }
+        record = self.q_one(sql, params)
+        if record is None:
+            return None
+        all_state_names = set(STATE_NAME_TO_ABBR.keys())
+        found = set(record['found'].split(','))
+        found.discard('')
+        self.log.debug(f'{found=}')
+        looking = all_state_names - found
+        self.log.debug(f'{looking=}')
+        return {
+            'found': found,
+            'game_id': game_id,
+            'looking': looking,
+            'started_at': record['started_at'].replace(tzinfo=datetime.timezone.utc),
+        }
+
+    def games_insert(self) -> uuid.UUID:
+        sql = '''
+            insert into games (found, game_id, started_at)
+            values ('', :game_id, :started_at)
+        '''
+        game_id = uuid.uuid4()
+        params = {
+            'game_id': game_id,
+            'started_at': datetime.datetime.utcnow(),
+        }
+        self.u(sql, params)
+        return game_id
+
+    def games_update(self, game_id: uuid.UUID, found: set):
+        sql = '''
+            update games
+            set found = :found
+            where game_id = :game_id
+        '''
+        params = {
+            'found': ','.join(sorted(found)),
+            'game_id': game_id,
+        }
+        self.u(sql, params)
 
     def db_migrate(self):
         self.log.info(f'Database schema version is {self.version}')
@@ -15,10 +78,38 @@ class Database(fort.SQLiteDatabase):
                 )
             ''')
             self.version = 1
+        if self.version < 2:
+            self.log.info('Migrating database to schema version 2')
+            self.u('''
+                create table states (
+                    state_name text,
+                    state_abbreviation text
+                )
+            ''')
+            sql = '''
+                insert into states (state_name, state_abbreviation)
+                values (:state_name, :state_abbreviation)
+            '''
+            params = [
+                {'state_name': k, 'state_abbreviation': v}
+                for k, v in STATE_NAME_TO_ABBR.items()
+            ]
+            self.b(sql, params)
+            self.version = 2
+        if self.version < 3:
+            self.log.info('Migrating database to schema version 3')
+            self.u('''
+                create table games (
+                    game_id uuid primary key,
+                    started_at timestamp,
+                    found text
+                )
+            ''')
+            self.version = 3
 
     def _table_exists(self, table_name: str) -> bool:
         sql = '''
-            select name from sqlite_schema where type = 'table' and name = :table_name
+            select name from sqlite_master where type = 'table' and name = :table_name
         '''
         params = {
             'table_name': table_name,
